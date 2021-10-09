85d3580193e9b0537315b4c07e2108a3c5c63f09
ushan121 <ushanshanilka80@gmail.com>
2021-10-09 08:58:18 +0530

    first commit

---
+++ src/lk/ijse/exam/AppInitializer.java
@@ -0,0 +1,19 @@
+package lk.ijse.exam;/**
+ * @author Ushan Shanilka <ushanshanilka80@gmail.com>
+ * @since 10/9/2021
+ **/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
